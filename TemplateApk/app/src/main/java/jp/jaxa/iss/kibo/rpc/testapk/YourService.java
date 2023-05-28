4e01df01b15060478500f8a601cae0c2a79847dd
jessia <sadiajessia@gmail.com>
2023-05-28 13:10:00 +0600

    import library

--- TemplateApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
+++ TemplateApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/testapk/YourService.java
@@ -1,6 +1,25 @@
 package jp.jaxa.iss.kibo.rpc.testapk;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+
+import org.opencv.aruco.Aruco;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gov.nasa.arc.astrobee.Result;
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
 /**
  * Class meant to handle commands from the Ground Data System and execute them in Astrobee
  */
